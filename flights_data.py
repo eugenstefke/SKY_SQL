467afddcf85503534bf4394292a98a12d8e32dab
Eugen Stefke <eugen.stefke@web.de>
2026-08-06 14:05:08 +0200

    search by delayed airport and airline works

--- flights_data.py
+++ flights_data.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine, text
 
 QUERY_FLIGHT_BY_ID = "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines ON flights.airline = airlines.id WHERE flights.ID = :id"
 QUERY_FLIGHT_BY_DATE = "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines ON flights.airline = airlines.id WHERE flights.DAY = :day AND flights.MONTH = :month AND flights.YEAR = :year"
+QUERY_DELAYED_FLIGHTS_BY_AIRPORT = "SELECT flights.*, airlines.airline, airports.AIRPORT, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines ON flights.airline = airlines.id JOIN airports ON flights.ORIGIN_AIRPORT = airports.IATA_CODE WHERE airports.IATA_CODE = :IATA_CODE"
+QUERY_DELAYED_FLIGHTS_BY_AIRLINE =  "SELECT flights.*, airlines.airline, airports.AIRPORT, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines ON flights.airline = airlines.id JOIN airports ON flights.ORIGIN_AIRPORT = airports.IATA_CODE WHERE airlines.AIRLINE = :AIRLINE"
 
 # Define the database URL
 DATABASE_URL = "sqlite:///data/flights.sqlite3"
@@ -44,3 +46,19 @@ def get_flights_by_date(day, month, year):
               'month' : month,
               'year' : year}
     return execute_query(QUERY_FLIGHT_BY_DATE, params)
+
+def get_delayed_flights_by_airport(IATA_CODE):
+    """
+    Searches for flight details using airport IATA_CODE.
+    If the flight was found, returns a list with a single record.
+    """
+    params = {'IATA_CODE': IATA_CODE}
+    return execute_query(QUERY_DELAYED_FLIGHTS_BY_AIRPORT, params)
+
+def get_delayed_flights_by_airline(airline):
+    """
+    Searches for flight details using airline name.
+    If the flight was found, returns a list with a single record.
+    """
+    params = {'AIRLINE': airline}
+    return execute_query(QUERY_DELAYED_FLIGHTS_BY_AIRLINE, params)
